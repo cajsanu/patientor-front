4c7c691376546148b09a6a9148bfa423bdd03d26
Cajsa Nummelin <cajsanummelin@Aleksandrs-Air.home>
2024-03-22 17:01:15 +0200

    Added base for component to see information about a single patient and updated implementation of reactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { Button, Divider, Container, Typography } from "@mui/material";
 
 import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
@@ -11,6 +10,7 @@ import PatientListPage from "./components/PatientListPage";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  console.log(patients);
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
@@ -21,23 +21,20 @@ const App = () => {
     };
     void fetchPatientList();
   }, []);
-  
+
   return (
     <div className="App">
-      <Router>
-        <Container>
-          <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
-            Patientor
-          </Typography>
-          <Button component={Link} to="/" variant="contained" color="primary">
-            Home
-          </Button>
-          <Divider hidden />
-          <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-          </Routes>
-        </Container>
-      </Router>
+      <Container>
+        <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
+          Patientor
+        </Typography>
+        <Button variant="contained" color="primary">
+          Home
+        </Button>
+        <Divider hidden />
+
+        <PatientListPage patients={patients} setPatients={setPatients} />
+      </Container>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,23 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import { SinglePatientPage } from "./components/SinglePatientPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "/patients/:id",
+    element: <SinglePatientPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
